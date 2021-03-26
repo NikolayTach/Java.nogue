2df821adb632608d9f237f00d9a0c0c97f7a74fe
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-03-26 07:47:25 +0000

    Update nogue.js

--- nogue.js
+++ nogue.js
@@ -1,44 +1,18 @@
 import function QACX.yml
 import function m21.yml
 import function ADSCVIVE.ts
-import AB function 
-import AB(1) function
+import JAVA.NOGUE function 
+import JAVA.NOGUE(!1) function
 {
     {
 A brings '11' and '22' 
-A by 1 number seq in .A
-take gor = brings [A.A.A.A.A.A]
+A by 1 number seq in JAVA.NOGUE
+take gor = brings [JAVA!NOGUE.JAVA!NOGUE2.JAVA!NOGUE3.AVA!NOGUE4.JAVA!NOGUE5.JAVA!NOGUE6]
 gor = 3 As B(11122) popups (nosign!!)
 gor = form !B folowing brings '11'
 B brings '22' and '11' 
-B by 1 number seq in .B
-take gor = brings [B.B.B.B.B.B]
-			    }	
-loop range as foward a beta "java.nogue.io"
-     freqent request file nogue
-   }
-      aquire late request to range /driver/foward0139284/glogue0.txt
-foward maintain qeued form QACX odd load vars saved 
-  roams as .io request latency //mail to nikolai.massine@gmail.com//
-def as
-    varies noodes all vague diffence dfp(111_222)
-def wave 
-    set function to of NULL dance when done  
-       madden /n
-local loving configuration_data set function queary loop.node 
-def stack = [ log01, log2, log3 ]
-"fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>
-fit moder when fix open than ps<java.nogue.io>"
-}  
+B by 1 number seq in JAVA.NOGUE(!1)
+take gor = brings [JAVA.NOGUE(!1).JAVA.NOGUE(!1)2.JAVA.NOGUE(!1)3.JAVA.NOGUE(!1)4.JAVA.NOGUE(!1)5.JAVA.NOGUE(!1)6]
+			 
 
-  aquire late request to range /driver/foward0139284/glogue1.txt 
-foward maintain qeued form m21 odd load vars saved 
-  roams as .io request latency of SDK;0
-as querry NULL open | retry $R 
 }
