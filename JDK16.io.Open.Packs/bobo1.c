9b38cb2984c8ab62ccd7b36105482cf4ca39314f
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-04-24 09:49:52 +0300

    Add files via upload

---
+++ JDK16.io.Open.Packs/bobo1.c
@@ -0,0 +1,20 @@
+#include <bobo1.sh> 
+{ 
+  back file "/main/xop-master/bobo1"  int  
+   do 
+     -i (function) brings vol%6
+     -b (function) brings vol%6
+'SF144' NULL
+do range  int -b 
+  'kolli' do NULL 
+    -a "/main/xop-master/bobo1.c"
+         while arg 'SF144' sooked del 'SF1123' for @  queary search -i  asc "java.nogue.io" load int  
+for  range NULL "main/xop-master/bobo1" so queary 
+arg int 
+  move face to v0:1616743818.9542642
+Done!
+ }
+   {
+number //-a , -b,-f ,-l ,-i//  
+
+}
