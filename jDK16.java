2eaf654fb96a89d325c9d5e15f8735d8868f8298
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-04-25 09:26:41 +0300

    Add files via upload

---
+++ jDK16.java
@@ -0,0 +1,10 @@
+{
+import function arrGuestList(01);extandedArrayFunction to NullFunction
+Path path = FileSystems.getDefault().getPAth("logs", "access.log");
+BufferedReader reader = Files.newBudderedREader(path, StandardCharsets.UTF_8);
+(++i < null ), 
+	} 
+  Request.UTF()Seen01.arrGuestList(01) 
+       Reach.Guest.List(01)
+                   [accsi.log1]
+}
