b659cdf721cc6342690a776216db6dcdc1e7b1c6
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-05-31 08:36:24 -0700

    Update jdk8.nix

--- jdk8.nix
+++ jdk8.nix
@@ -2,7 +2,7 @@
 let 
   myjdk = pkgs.jdk8.override {
     cacert = pkgs.runCommand "mycacert" {} ''
-      mkdir -p @nogue.io/oji/jvm-mgr;1
+      mkdir -p @nogue.io/oji/jvm-mgr;
       cat ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt \
         ${./my-company-root-certificate.crt} > $out/etc/ssl/certs/ca-bundle.crt
     '';
