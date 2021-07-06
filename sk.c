b01573e9e8477abbab5f7b108e69c2acb7e7693e
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-07-05 23:04:27 -0700

    Update sk.c

--- sk.c
+++ sk.c
@@ -5,4 +5,5 @@
 RYTHM1_ON = setvalue(1)
 	MAX_ME = R(ON)
 
-#Include the java.nogue.io domians that are proned to the JDK16 beta verison related issues must be opened in the following options, that open the public signatures overwrites.as based values of NIX_OS
+#Include the java.nogue.io domians that are proned to the JDK16 beta verison related issues must be opened in the following options,
+#that open the public signatures overwrites.as based values of NIX_OS.
