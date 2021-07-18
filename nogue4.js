a62fa3a55f03a4e2829c55394cb431740f51ebb6
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-07-18 19:03:30 +0000

    Add files via upload

---
+++ nogue4.js
@@ -0,0 +1,19 @@
+import from loader1@nogue.io
+import from data.nerve@nogue.io
+	{
+	@nogue.io prone 
+		as 
+		one form to 
+	node_link = [WORKSHOP, JAVA SE 16 DIR, NOGUE PACK DIR]
+color for   	
+"java.nogue.io" main title workload 
+inquery form 
+		}
+/n
+	{
+	list full option 
+		as
+		 2 pictures sets to null dir 	
+	set to (12.13.14.15) as 
+nogue.json with public_key:
+}
