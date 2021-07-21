7875be57221d98c79fa967698ef5edcbaf3beaa6
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-07-21 10:31:44 -0700

    Update nogue4.js

--- nogue4.js
+++ nogue4.js
@@ -17,19 +17,19 @@ inquery form
 	set to (12.13.14.15) as 
 nogue.json with public_key:
 	init	: function() { 
-		if (navigator.mimeTypes[gJavaNogueConsole1_8_0_291.mimeType]) {
+		if (navigator.mimeTypes["gJavaNogueConsole1_8_0_291.mimeType"]) {
 			var toolsPopup = document.getElementById("menu_ToolsPopup");	
-			toolsPopup.addEventListener("popupshowing",gJavaNogueConsole1_8_0_291.enable,false);
-			var element = document.getElementById(gJavaNogueConsole1_8_0_291.id);
-			element.setAttribute( "oncommand" , "gJavaConsole1_8_0_291.show();");
+			toolsPopup.addEventListener("popupshowing","gJavaNogueConsole1_8_0_291".enable,false);
+			var element = document.getElementById("gJavaNogueConsole1_8_0_291".id);
+			element.setAttribute( "oncommand" , "gJavaNogueConsole1_8_0_291.show();");
 		} else {
-			var element = document.getElementById(gJavaNogueConsole1_8_0_291.id);
+			var element = document.getElementById("gJavaNogueConsole1_8_0_291".id);
 			element.setAttribute("style", "display: none");
 		}
 	},
 
 	enable	: function() {
-		var element = document.getElementById(gJavaNogueConsole1_8_0_291.id);
+		var element = document.getElementById("gJavaNogueConsole1_8_0_291".id);
     		if (navigator.javaEnabled()) {
 			element.removeAttribute("disabled");
     		} else {
@@ -45,7 +45,7 @@ nogue.json with public_key:
 	
 };
 
-gJavaNogueConsole1_8_0_291.install();
+"gJavaNogueConsole1_8_0_291".install();
 
 © 2021 GitHub, Inc.
 }
