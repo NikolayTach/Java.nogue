877c9d5d32f8f098e95c2b2a6d4d482345d4d183
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-07-28 17:05:30 +0000

    Add files via upload

---
+++ .github/Aceofspades.h
@@ -0,0 +1,22 @@
+#include<bar.ch>
+
+as wheel
+single  
+	tune = [1,2,3,4,5,6,7,8,9,10]
+	while locks
+do
+	int 
+		'/var/media/nixos' 
+	if
+:nogue: ling to 
+null 
+
+dual 
+	tune = [1,2,3,4,5,6,7,8,9,10]
+	while locks
+do
+	int 
+		'/var/media/nixos' 
+	if
+:nogue:
+Done! 
